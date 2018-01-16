7ca209753083c7b3581b6eebdb0316ba07a04431
jonlemur <jonpirat@gmail.com>
2018-01-16 18:48:49 +0100

    boiler tk

---
+++ boiler01.py
@@ -0,0 +1,27 @@
+'''
+boiler plate for mini tk app with subthread
+By: Jon Aspeheim
+'''
+
+import threading
+import time
+from Tkinter import *
+
+##Use for subthread
+def worker():
+    while True:
+        print 'Hello'
+        time.sleep(2)
+
+# Main function - Startup
+def main():
+    myThread = threading.Thread(target=worker)
+    myThread.start()
+
+    root = Tk()
+    w = Label(root, text="Hello, world!")
+    w.pack()
+    root.mainloop()
+
+if __name__ == '__main__':
+  main()
